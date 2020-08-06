70363a87e06666094619508de0c2003823a3a916
Gavin Grant <gavingrant@Gavins-MacBook-Pro.local>
2020-08-06 10:49:51 -0700

    controlling the script with arguments

--- animate-string.js
+++ animate-string.js
@@ -1,9 +1,17 @@
 const chalk = require('chalk-animation');
 
-function animateString(string) {
-    const animation = chalk.rainbow(string);
+// list valid methods
+const validMethods = [ `glitch`, `karaoke`, `neon`, `pulse`, `radar`, `rainbow` ];
+// grab third position in args array
+const scriptArg = process.argv[2];
+// check the arg is valid and use it or default to 'rainbow'
+const method = validMethods.includes(scriptArg) ? scriptArg : 'rainbow';
 
-    setTimeout(() => animation.stop(), 1300);
+function animateString(string) {
+    // use the method that was selected
+    const animation = chalk[method](string);
+
+    setTimeout(() => animation.stop(), 2000);
 }
 
 module.exports = { animateString: animateString }; // named export
